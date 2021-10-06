8b58f77139ad3e78131c2757048933cbb64fe141
Yuriy Skachkov <vaisrul@yandex.ru>
2021-10-06 12:35:56 +0300

    Upload game

---
+++ CardGame/Assets/MemoryCard.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    public SceneController controller;
+    private int _id;
+    public int id { get { return _id; } }
+
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf && controller.canReveal)
+        {
+            cardBack.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+
+
+}
